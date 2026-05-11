0dccfe28055a7aedfee930e1c1e3b458214754bc
nom <nom@XUL-M2BA.local>
2026-05-11 17:46:13 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -e
+
+sudo apt-get update
+sudo apt-get install -y neovim stow zoxide eza git curl
+
+rm -f ~/.zshrc
+
 cd "$HOME/dotfiles"
-stow tmux nvim zsh alacritty
+stow -v -t "$HOME" tmux nvim linuxzsh alacritty
