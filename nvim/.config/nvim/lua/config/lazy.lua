07bb6aba81764a547f45b883a04aa209460a93d9
nom <nom@XUL-M2BA.local>
2026-05-13 21:40:37 +0200

    Remove sidekick, add Copilot in Lazy.lua

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -18,8 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- AI: Copilot autocomplete + sidekick
-    { import = "lazyvim.plugins.extras.ai.sidekick" },
+    -- AI: Copilot autocomplete
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
